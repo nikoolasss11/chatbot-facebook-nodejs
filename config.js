c6f869358f2bd43e06750c628bbc2cd488ddfa5f
nikoolasss11 <nikolas.menicou@gmail.com>
2017-11-19 12:24:21 +0200

    Initial configuration

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
     FB_VERIFY_TOKEN: 'n2468642n8tz!pfb5',
     API_AI_CLIENT_ACCESS_TOKEN: '143b4c0cb3f3421b9485177b63a4b5cb',
     FB_APP_SECRET: '1adb32fd582fe0f257f88199f917fdcc',
-    SERVER_URL: "https://testbot01111992.herokuapp.com/",
+    SERVER_URL: "http://testbot01111992.herokuapp.com/",
 };
